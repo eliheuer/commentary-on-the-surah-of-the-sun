b419700307291f7d62df4f51921b495e1b1ba83a
Eli Heuer <eliheuer@gmail.com>
2018-11-13 01:42:17 -0500

    Adding DrawBot cover source file

--- drawbot-cover-source.py
+++ drawbot-cover-source.py
@@ -5,49 +5,52 @@ import os
 
 
 # STATIC VARIABLE
-W,H,M,F = 792,612,64,1         # WIDTH, HEIGHT, MARGIN, FRAMES
+W,H,M,F = 792,612,18,1         # WIDTH, HEIGHT, MARGIN, FRAMES
 VAR_WGHT = 100                 # VARIABLE FONT WEIGHT
 LINE_H = H/20                  # LINE HEIGHT
-START_POS = 400
+START_POS = 495
 
 
 # SET FONT
-font("fonts/Inter-UI-VF.ttf")
+font("Helvetica Neue Bold")
 for axis, data in listFontVariations().items():
     print((axis, data))
 
 # GRID DRAWING FUNCTION
 def grid(inc):
-    stroke(0.8)
-    stpX, stpY = 0, 0
-    incX = (W-(M*2))/inc
-    incY = (W-(M*2))/inc
-    print("incX=", incX)
-    for x in range(inc+1):
-        polygon((M+stpX, M), (M+stpX, H-M))
-        stpX += incX
-    for y in range(inc+1):
-        polygon((M, M+stpY), (H-M, M+stpY))
+    stroke(0, 0, 1, 0.2)
+    stpY = 0
+    incY = 18
+#    for x in range(inc+1):
+#        polygon((M+stpX, M), (M+stpX, H-M))
+#        stpX += incX
+    for y in range(34):
+        polygon((0, stpY), (W, stpY))
         stpY += incY
 
 # DRAW NEW PAGE
 newPage(W, H)
-fill(1)
+fill(0.8)
 rect(0, 0, W, H)
 grid(20)
 
 #Guides
-stroke(1,0,0)
+stroke(1,0,0,0.5)
+strokeWidth(2)
 polygon((W/2, 0),(W/2, H))
+polygon((W/2-(M*2), 0),(W/2-(M*2), H))
+polygon((W/2+(M*2), 0),(W/2+(M*2), H))
+polygon((M*2, 0),(M*2, H))
+polygon((W-M*2, 0),(W-M*2, H))
 
 # HEADLINE
 fill(0)
-tracking(0)
+#tracking(-1.5)
 stroke(None)
-fontSize(24)
+fontSize(34)
 fontVariations(wght=900)
-text("Commentary on the  ",      (W/2+M, (START_POS)-(0*LINE_H)))
-text("Surah of the Sun ",       (W/2+M, (START_POS)-(1*LINE_H)))
+text("Commentary on the",         (W/2+(M*2), (29*M)))
+text("Surah of the Sun",       (W/2+(M*2), (27*M)))
 
 # Draw large type
 fontSize(50)
@@ -58,15 +61,17 @@ for i in range(6):
 #    text("Variable Font", (M+10, (START_POS-M+3)-(i*40)))
 
 # Dot
-fill(1,0,0)
-oval(M, M, (W-(M*2))/20, (W-(M*2))/20)
+fill(1,0.5,0)
+stroke(1,0.5,0)
+
+# X,Y,W,H
+oval( (W/2)+(M*2) , (LINE_H*4)+2-(M*2) , (W/2)-(M*4) , (W/2)-(M*4) )
 
 # Draw small type
 fontVariations(wght=400)
 fill(1)
 fontSize(50)
 tracking(-4)
-text("A B C D E F G H I J ",      (M+440, (START_POS)-( 8*LINE_H/2)))
 
 # Save PDF
-saveImage("cover-design.pdf")
+saveImage("cover-design.png")
